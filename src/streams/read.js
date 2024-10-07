6c41c05d84a28c1f191a13f9e9c4b4b3392c5a20
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-10-07 19:34:11 +0400

    feat: add streams read task solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import { open } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const dirrPath = process.cwd();
+    const filePath = path.join(dirrPath, 'files', 'fileToRead.txt');
+    const stream = await open(filePath);
+    const readStream = stream.createReadStream();
+    const result = [];
+    readStream.on('data', chunk => result.push(chunk));
+    readStream.on('close', () => {
+        process.stdout.write(result.join())
+    });
 };
 
 await read();
