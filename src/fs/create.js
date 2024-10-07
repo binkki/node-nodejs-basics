cb1e1966d41253a56b4d303b06067b9472504f1d
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-10-07 18:39:07 +0400

    feat: add fs create task solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import { writeFile } from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+    const fileText = 'I am fresh and young';
+    const fileName = 'fresh.txt';
+    const dirrPath = process.cwd();
+    const filePath = path.join(dirrPath, 'files', fileName);
+    try {
+        await writeFile(
+            filePath,
+            fileText,
+            { flag: 'wx' }
+        );
+    }
+    catch(error) {
+        console.error(new Error('FS operation failed'));
+    }    
 };
 
 await create();
