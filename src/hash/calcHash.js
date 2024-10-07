27e558d21b62468270466473642de5997c1d9563
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-10-07 20:20:46 +0400

    feat: add hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { createHash } from "node:crypto";
+import { open } from 'node:fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const dirrPath = process.cwd();
+    const filePath = path.join(dirrPath, 'files', 'fileToCalculateHashFor.txt');
+    const stream = await open(filePath);
+    const readStream = stream.createReadStream();
+    const fileText = [];
+    readStream.on('data', chunk => fileText.push(chunk));
+    readStream.on('close', () => {
+        const hashText = createHash("sha256").update(fileText.join()).digest('hex');
+        process.stdout.write(hashText);
+    });
 };
 
 await calculateHash();
